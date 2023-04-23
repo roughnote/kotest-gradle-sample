752fd78154a0512b7c53226d8bbeffb7b3253b34
Pradeep Murugesan <pradeep.murugesan@transferwise.com>
2023-04-23 06:46:41 +0100

    remove test containers references

--- src/test/kotlin/com/pradeep/databaselearning/DatabaseLearningApplicationTests.kt
+++ src/test/kotlin/com/pradeep/databaselearning/DatabaseLearningApplicationTests.kt
@@ -2,11 +2,8 @@ package com.pradeep.databaselearning
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ContextConfiguration
-import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
-@ContextConfiguration(initializers = [TestContainersInitializer::class])
 class DatabaseLearningApplicationTests {
 
 	@Test
